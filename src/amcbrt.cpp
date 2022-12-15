401e23576f38ff380f82d64fbae25922d216555c
John Yannotty <jcyannotty@gmail.com>
2022-12-15 09:46:04 -0500

    Create amcbrt class

---
+++ src/amcbrt.cpp
@@ -0,0 +1,201 @@
+#include "amcbrt.h"
+#include "brtfuns.h"
+#include <iostream>
+#include <map>
+#include <vector>
+
+using std::cout;
+using std::endl;
+
+//--------------------------------------------------
+//a single iteration of the MCMC for brt model
+void amcbrt::drawvec(rn& gen){
+    for(size_t j=0;j<m;j++) {
+        //Uses operators defined in dinfo on y to compute the jth residual Rij
+        *divec[j] = *di;
+        *divec[j]-= *getf(); //Yi - sum_k g(xi, Tk, Mk) 
+        *divec[j]+= *mb[j].getf(); //Computes the jth residual in BART model
+
+        //Draw parameter vector in the jth tree
+        mb[j].drawvec(gen);
+        
+        // Update the in-sample predicted vector
+        setf_vec();
+
+        // Update the in-sample residual vector
+        setr_vec();
+    }
+    // overall statistics from the subtrees.  Need to divide by m*N to get
+    // useful numbers after the MCMC is done.
+    if(mi.dostats) {
+    resetstats();
+    for(size_t j=0;j<m;j++){
+        mb[j].addstats(mi.varcount,&mi.tavgd,&mi.tmaxd,&mi.tmind);
+    }
+  }
+}
+
+//--------------------------------------------------
+//slave controller for draw when using MPI
+void amcbrt::drawvec_mpislave(rn& gen){
+    for(size_t j=0;j<m;j++) {
+        //Get the jth resiudal
+        *divec[j]= *di;
+        *divec[j]-= *getf();
+        *divec[j]+= *mb[j].getf();
+
+        // do the draw for jth component
+        mb[j].drawvec_mpislave(gen);
+        
+        // Update the in-sample predicted vector
+        setf_vec();
+
+        // Update the in-sample residual vector
+        setr_vec();
+    }
+}
+
+//--------------------------------------------------
+//adapt the proposal widths for perturb proposals,
+//bd or rot proposals and b or d proposals.
+void amcbrt::adapt(){
+  for(size_t j=0;j<m;j++) {
+#ifndef SILENT
+    cout << "\nAdapt ambrt[" << j << "]:";
+#endif
+    mb[j].adapt();
+  }
+}
+
+//--------------------------------------------------
+//setdata for amxbrt
+void amcbrt::setdata_vec(dinfo *di) {
+    this->di=di;
+        
+    // initialize notjsigmavs.
+    for(size_t j=0;j<m;j++){
+        notjmus[j].resize(this->di->n,0.0);
+    }
+    for(size_t j=0;j<m;j++){
+        for(size_t i=0;i<di->n;i++){
+            //notjmus[j][i]=this->di->y[i]/((double)m);
+            notjmus[j][i]=this->di->y[i];
+        }
+    }
+    for(size_t j=0;j<m;j++){
+        divec[j]=new dinfo(this->di->p,this->di->n,this->di->x,&notjmus[j][0],this->di->tc); //constructing a new dinfo with notjmus[j][0] as the y value 
+    }
+
+    // each mb[j]'s data is the appropriate row in notjmus
+    for(size_t j=0;j<m;j++){
+        mb[j].setdata_vec(divec[j]); //setdata_vec is a method of mb[j] which is a member of mxbrt class. This is different than setdata_vec in mxbrt
+    }
+    resid.resize(di->n);
+    yhat.resize(di->n);
+    setf_vec();
+    setr_vec();
+}
+
+//--------------------------------------------------
+//set vector of predicted values for psbrt model
+void amcbrt::local_setf_vec(diterator& diter){
+   for(;diter<diter.until();diter++){
+      yhat[*diter]=0.0;
+      for(size_t j=0;j<m;j++)
+        yhat[*diter]+=mb[j].f(*diter); //sum of trees - add the fitted value from each tree
+   }
+}
+
+//--------------------------------------------------
+//set vector of residuals for psbrt model
+void amcbrt::local_setr_vec(diterator& diter){
+   for(;diter<diter.until();diter++) {
+      resid[*diter]=di->y[*diter]-f(*diter);
+   }
+}
+
+//--------------------------------------------------
+//predict the response at the (npred x p) input matrix *x
+//Note: the result appears in *dipred.y.
+void amcbrt::local_predict_vec(diterator& diter, finfo& fipred){
+    tree::tree_p bn;
+    double temp;
+    vxd thetavec_temp(k); 
+    for(;diter<diter.until();diter++) {
+        //Initialize to zero 
+        temp = 0;
+        thetavec_temp = vxd::Zero(k);
+        
+        for(size_t j=0;j<m;j++) {
+            bn = mb[j].t.bn(diter.getxp(),*xi);
+            thetavec_temp = bn->getthetavec();
+            temp = temp + fipred.row(*diter)*thetavec_temp;
+        }
+        //std::cout << temp << std::endl;
+        diter.sety(temp);
+    }
+}
+
+//--------------------------------------------------
+//Local Save tree
+void amcbrt::local_savetree_vec(size_t iter, int beg, int end, std::vector<int>& nn, std::vector<std::vector<int> >& id, 
+    std::vector<std::vector<int> >& v, std::vector<std::vector<int> >& c, std::vector<std::vector<double> >& theta){
+    size_t indx=iter*m;
+    for(size_t i=(indx+(size_t)beg);i<(indx+(size_t)end);i++) {
+        nn[i]=mb[i-indx].t.treesize();
+        id[i].resize(nn[i]);
+        v[i].resize(nn[i]);
+        c[i].resize(nn[i]);
+        theta[i].resize(k*nn[i]);
+        mb[i-indx].t.treetovec(&id[i][0],&v[i][0],&c[i][0],&theta[i][0],k);
+    }
+}
+
+//--------------------------------------------------
+//Local load tree
+void amcbrt::local_loadtree_vec(size_t iter, int beg, int end, std::vector<int>& nn, std::vector<std::vector<int> >& id, std::vector<std::vector<int> >& v,
+                  std::vector<std::vector<int> >& c, std::vector<std::vector<double> >& theta){
+  size_t indx=iter*m;
+  for(size_t i=(indx+(size_t)beg);i<(indx+(size_t)end);i++)
+    mb[i-indx].t.vectotree(nn[i],&id[i][0],&v[i][0],&c[i][0],&theta[i][0],k);
+}
+
+//--------------------------------------------------
+//pr for brt
+void amcbrt::pr_vec()
+{
+   std::cout << "***** amcbrt object:\n";
+   cout << "Number of trees in product representation:" << endl;
+   cout << "        m:   m=" << m << endl;
+   cout << "Conditioning info on each individual tree:" << endl;
+   cout << "   mean:   tau1=" << ci.tau1 << endl;
+   cout << "   mean:   mu1 =" << ci.mu1 << endl;
+   cout << "   mean:   tau2=" << ci.tau1 << endl;
+   cout << "   mean:   mu2 =" << ci.mu1 << endl;
+   if(!ci.sigma)
+     cout << "         sigma=[]" << endl;
+   else
+     cout << "         sigma=[" << ci.sigma[0] << ",...," << ci.sigma[di->n-1] << "]" << endl;
+   brt::pr_vec();
+   cout << "**************Trees in sum representation*************:" << endl;
+   for(size_t j=0;j<m;j++) mb[j].t.pr_vec();
+}
+
+//--------------------------------------------------
+//Collapse BART ensemble into one supertree
+//The single supertree created will be stored in st.
+void amcbrt::collapseensemble()
+{
+   tree::npv bots;
+   resetst();
+
+   for(size_t j=1;j<m;j++)
+   {
+      st.getbots(bots);
+      //collapse each tree j=1..m into the supertree
+      for(size_t i=0;i<bots.size();i++)
+         collapsetree_vec(st,bots[i],this->gettree(j)); //mb[j]->t);
+      bots.clear();
+   }
+
+}
---
+++ src/amcbrt.h
@@ -0,0 +1,87 @@
+#ifndef GUARD_amcbrt_h
+#define GUARD_amcbrt_h
+
+#include "tree.h"
+#include "treefuns.h"
+#include "dinfo.h"
+#include "mcbrt.h"
+
+class amcbrt : public mcbrt {
+public:
+   //--------------------
+   //classes
+   // tprior and mcmcinfo are same as in brt
+   // cinfo same as in mxbrt
+   //--------------------
+   //constructors/destructors
+   amcbrt(): mcbrt(),st(0),m(200),mb(m),notjmus(m),divec(m) {}
+   amcbrt(size_t im): mcbrt(),st(0),m(im),mb(m),notjmus(m),divec(m) {}
+   //amxbrt(size_t im, size_t ik): mxbrt(ik),st(0),m(im),mb(m),notjmus(m),divec(m) {}
+   virtual ~amcbrt() {
+      if(!notjmus.empty()) {
+         for(size_t j=0;j<m;j++) notjmus[j].clear();
+         notjmus.clear();
+         for(size_t j=0;j<m;j++) delete divec[j];
+      }
+      st.tonull();
+   }
+
+   //--------------------
+   //methods
+   void drawvec(rn& gen);
+   void drawvec_mpislave(rn& gen);
+   void adapt();
+   void setmpirank(int rank) { this->rank = rank; for(size_t j=0;j<m;j++) mb[j].setmpirank(rank); }  //only needed for MPI
+   void setmpicvrange(int* lwr, int* upr) { this->chv_lwr=lwr; this->chv_upr=upr; for(size_t j=0;j<m;j++) mb[j].setmpicvrange(lwr,upr); } //only needed for MPI
+   void setci(double mu1, double mu2, double tau1, double tau2, double* sigma) { ci.tau1=tau1;ci.tau2=tau2;ci.mu1=mu1;ci.mu2=mu2;ci.sigma=sigma;
+                for(size_t j=0;j<m;j++) mb[j].setci(tau1,tau2,mu1,mu2,sigma); }
+   void settc(int tc) { this->tc = tc; for(size_t j=0;j<m;j++) mb[j].settc(tc); }
+   void setxi(xinfo *xi) { this->xi=xi; for(size_t j=0;j<m;j++) mb[j].setxi(xi); }
+   void setfi(finfo *fi, size_t k) {this->fi = fi; this->k = k; this-> nsprior = false ;for(size_t j=0;j<m;j++) mb[j].setfi(fi,k); }
+   void setdata_vec(dinfo *di);
+   void settp(double alpha, double beta) { tp.alpha=alpha;tp.beta=beta; for(size_t j=0;j<m;j++) mb[j].settp(alpha,beta); }
+   tree::tree_p gettree(size_t i) { return &mb[i].t; }
+   void setmi(double pbd, double pb, size_t minperbot, bool dopert, double pertalpha, double pchgv, std::vector<std::vector<double> >* chgv)
+             { mi.pbd=pbd; mi.pb=pb; mi.minperbot=minperbot; mi.dopert=dopert;
+               mi.pertalpha=pertalpha; mi.pchgv=pchgv; mi.corv=chgv; 
+               for(size_t j=0;j<m;j++) mb[j].setmi(pbd,pb,minperbot,dopert,pertalpha,pchgv,chgv); }
+   void setstats(bool dostats) { mi.dostats=dostats; for(size_t j=0;j<m;j++) mb[j].setstats(dostats); if(dostats) mi.varcount=new unsigned int[xi->size()]; }
+   void pr_vec();
+   // drawnodetheta, lm, add_observation_to_suff and newsinfo/newsinfovec unused here.
+
+   // convert BART ensemble to single supertree
+   void resetst() { st.tonull(); st=mb[0].t; } //copy mb0's tree to st.
+   void collapseensemble();
+
+    //--------------------
+    //data
+    //--------------------------------------------------
+    //stuff that maybe should be protected
+    tree st;
+
+protected:
+    //--------------------
+    //model information
+    size_t m;  //number of trees in sum representation
+    std::vector<mcbrt> mb;  // the vector of individual mu trees for sum representation
+    //--------------------
+    //data
+    std::vector<std::vector<double> > notjmus;
+    std::vector<dinfo*> divec;
+    //--------------------
+    //mcmc info
+    //--------------------
+    //methods
+    virtual void local_setf_vec(diterator& diter);  //set the vector of predicted values
+    virtual void local_setr_vec(diterator& diter);  //set the vector of residuals
+    virtual void local_predict_vec(diterator& diter, finfo& fipred); // predict y at the (npred x p) settings *di.x
+    virtual void local_get_mix_wts(diterator& diter, mxd& wts); // extract model weights at each *di.x settings
+    virtual void local_get_mix_theta(diterator& diter, mxd& wts); // extract the terminal node parameters for the first *di.x settings
+    virtual void local_savetree_vec(size_t iter, int beg, int end, std::vector<int>& nn, std::vector<std::vector<int> >& id, std::vector<std::vector<int> >& v,
+                    std::vector<std::vector<int> >& c, std::vector<std::vector<double> >& theta);
+    virtual void local_loadtree_vec(size_t iter, int beg, int end, std::vector<int>& nn, std::vector<std::vector<int> >& id, std::vector<std::vector<int> >& v,
+                    std::vector<std::vector<int> >& c, std::vector<std::vector<double> >& theta);
+
+};
+
+#endif
