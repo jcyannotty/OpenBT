88d8461fc0560b76ce4fe592801a1d13a0aafa42
John Yannotty <jcyannotty@gmail.com>
2022-12-25 20:18:01 -0500

    add parameters files

---
+++ src/parameters.cpp
@@ -0,0 +1,219 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstddef>
+#include <vector>
+
+#include "rn.h"
+#include "crn.h"
+#include "parameters.h"
+
+using std::cout;
+using std::endl;
+
+//----------------------------------------------
+// Adapt step for proposals
+void param::adapt(){
+    double accrate;
+    for(size_t j=0;j<p;j++) {
+        if(rank==0) std::cout << "Acceptance rate=" << ((double)acceptvec[j])/((double)(acceptvec[j]+rejectvec[j]));
+        accrate=((double)acceptvec[j])/((double)(acceptvec[j]+rejectvec[j]));
+        if(accrate>0.29 || accrate<0.19) propvec[j]*=accrate/0.24;
+        if(rank==0) std::cout << " (adapted propwidth to " << propvec[j] << ")" << std::endl;
+    }
+    std::fill(acceptvec.begin(),acceptvec.end(),0);
+    std::fill(rejectvec.begin(),rejectvec.end(),0);
+}
+         
+//----------------------------------------------
+// Draw new parameter -- marginal update
+void param::drawnew(size_t ind, rn &gen){
+    double u0;
+    u0 = 0;
+    cout << "here by mistake" << endl;
+}
+
+//----------------------------------------------
+// Draw new prametet -- joint update
+void param::drawnew(rn &gen){
+    unew.clear();
+    unew.resize(p);
+    for(size_t i=0;i<p;i++){
+        if(propdistvec[i] == "uniform"){
+            unew[i] = ucur[i] + (gen.uniform() - 0.5)*propvec[i];
+            //std::cout << "unew[i] = " << unew[i] << std::endl; 
+        }else if(propdistvec[i] == "normal"){
+            unew[i] = ucur[i] + gen.normal()*propvec[i];
+        }
+    }
+    // Pass the unew vector (as an array) if using mpi  
+#ifdef _OPENMPI
+    if(rank==0){ //should always be true when using mpi
+        char buffer[SIZE_UINT3*p];
+        int position=0;
+        MPI_Request *request=new MPI_Request[tc];
+        const int tag=MPI_TAG_CP_PASSUNEW;
+        double unarray[p];
+
+        // Cast vector to array
+        copy(unew.begin(),unew.end(),unarray);
+
+        // Pack and send info to the slaves
+        MPI_Pack(&unarray,p,MPI_DOUBLE,buffer,SIZE_UINT3*p,&position,MPI_COMM_WORLD);
+        for(size_t i=1; i<=(size_t)tc; i++) {
+            MPI_Isend(buffer,SIZE_UINT3*p,MPI_PACKED,i,tag,MPI_COMM_WORLD,&request[i-1]);
+        }
+        MPI_Waitall(tc,request,MPI_STATUSES_IGNORE);
+
+        delete[] request;
+    }
+#endif
+}    
+ 
+
+//----------------------------------------------
+// drawnew_mpi
+void param::drawnew_mpi(rn &gen){
+#ifdef _OPENMPI
+    int buffer_size = SIZE_UINT3*p;
+    char buffer[buffer_size];
+    int position=0;
+    MPI_Status status;
+ 
+    // MPI receive the proposed u vector.
+    MPI_Recv(buffer,buffer_size,MPI_PACKED,0,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+    double unarray[p];
+    MPI_Unpack(buffer,buffer_size,&position,&unarray,p,MPI_DOUBLE,MPI_COMM_WORLD);
+ 
+    unew.clear();
+    for(size_t j=0;j<p;j++){unew.push_back(unarray[j]);}
+#endif    
+}
+
+
+//----------------------------------------------
+// lm for calibration
+double param::lm(double cursumwr2, double newsumwr2){
+    double lprior = 0.0, ltemp = 0.0;
+    double lalpha;
+
+    // Get the log likelihood ratio
+    lalpha = 0.5*(cursumwr2 - newsumwr2);
+
+    // Get the logprior ratio
+    for(size_t i=0;i<p;i++){
+        if(propdistvec[i]=="uniform"){
+            lprior += 0.0;
+            if(unew[i]<priorp1vec[i] || unew[i]>priorp2vec[i]){
+                lprior=-std::numeric_limits<double>::infinity();
+            }
+        }else if(propdistvec[i]=="uniform"){
+            lprior += 0.5*(ucur[i]-priorp1vec[i])*(ucur[i]-priorp1vec[i])/(priorp2vec[i]*priorp2vec[i]);
+            lprior += -0.5*(unew[i]-priorp1vec[i])*(unew[i]-priorp1vec[i])/(priorp2vec[i]*priorp2vec[i]);
+        }
+    }
+    // Get the output lm
+    return lprior + lalpha;
+}
+
+//----------------------------------------------
+// MH step
+void param::mhstep(double csumwr2, double nsumwr2, rn &gen){
+#ifdef _OPENMPI
+    if(rank>0){
+        // Sum the suff stats then get accept/reject status
+        MPI_Status status;
+        MPI_Reduce(&csumwr2,NULL,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+        MPI_Reduce(&nsumwr2,NULL,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+        MPI_Recv(NULL,0,MPI_PACKED,0,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+        // Carry out the accept/reject step
+        if(status.MPI_TAG==MPI_TAG_CP_ACCEPT) {
+            for(size_t j=0;j<p;j++) acceptvec[j]++;
+            ucur = unew;
+            accept=true;
+        }else{
+            for(size_t j=0;j<p;j++) rejectvec[j]++;
+            accept=false;
+        }
+    }           
+
+    if(rank==0){
+        // Reduce the suff stats
+        MPI_Request *request = new MPI_Request[tc];
+        MPI_Reduce(MPI_IN_PLACE,&csumwr2,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+        MPI_Reduce(MPI_IN_PLACE,&nsumwr2,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+
+        // Do mhstep
+        double lmout = lm(csumwr2,nsumwr2);
+        double alpha = gen.uniform();
+        if(log(alpha)<lmout){
+            // accept
+            ucur = unew;
+            for(size_t j=0;j<p;j++) acceptvec[j]++;
+            const int tag=MPI_TAG_CP_ACCEPT;
+            for(size_t k=1; k<=(size_t)tc; k++){
+                MPI_Isend(NULL,0,MPI_PACKED,k,tag,MPI_COMM_WORLD,&request[k-1]);
+            }
+        }else{ 
+            // reject
+            const int tag=MPI_TAG_CP_REJECT;
+            for(size_t k=1; k<=(size_t)tc; k++) {
+                MPI_Isend(NULL,0,MPI_PACKED,k,tag,MPI_COMM_WORLD,&request[k-1]);
+            }
+            for(size_t j=0;j<p;j++) rejectvec[j]++;
+        }
+        MPI_Waitall(tc,request,MPI_STATUSES_IGNORE);
+        delete[] request;
+    }
+#else
+    // Do mhstep
+    double lmout = lm(csumwr2,nsumwr2);
+    double alpha=gen.uniform();
+    if(log(alpha)<lmout){
+        // accept
+        ucur = unew;
+        accept=true;
+        for(size_t j=0;j<p;j++) acceptvec[j]++;
+    }else{ 
+        // reject
+        accept=false;
+        for(size_t j=0;j<p;j++) rejectvec[j]++;
+    }
+#endif
+}
+
+//----------------------------------------------
+// Set priors
+void param::setpriors(std::vector<std::string> prdist, std::vector<double> prp1, std::vector<double> prp2){
+    for(size_t i=0;i<prdist.size();i++){
+        priordistvec.push_back(prdist[i]);
+        priorp1vec.push_back(prp1[i]);
+        priorp2vec.push_back(prp2[i]);
+    }
+}
+
+//----------------------------------------------
+// Set proposals
+void param::setproposals(std::vector<std::string> propdist, std::vector<double> prop){
+    for(size_t i=0;i<propdist.size();i++){
+        propdistvec.push_back(propdist[i]);
+        propvec.push_back(prop[i]);
+    }
+}
+
+//----------------------------------------------
+// Update x vector with unew, assumes the ucolumns are after all the regular x columns
+// ucols = the index of each u parameter (0,1,2,...,p), given p+1 total parameters
+void param::updatex(std::vector<double> &x, std::vector<size_t> ucols, size_t pxu, size_t n){
+    size_t uidx = pxu - ucols.size();
+    size_t uc;
+    //cout << "pxu = " << pxu << endl;
+    for(size_t i=0;i<n;i++){
+        for(size_t j=0;j<ucols.size();j++){
+            uc = ucols[j];
+            //cout << "xold = " << x[i*pxu + uc] << endl;
+            x[i*pxu + uc] = unew[j];
+            //cout << "xnew = " << x[i*pxu + uc] << endl;
+        }
+    }
+}
---
+++ src/parameters.h
@@ -0,0 +1,71 @@
+// Creates a parameters class for calibration (and potentially other models/settings) where
+// one needs to learn a parameter
+
+#ifndef GUARD_parameters_h
+#define GUARD_parameters_h
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstddef>
+#include <vector>
+
+#include "rn.h"
+
+#ifdef _OPENMPI
+#   include <mpi.h>
+#   include <crn.h>
+#   define SIZE_UINT1 16  // sizeof(unsigned int)
+#   define SIZE_UINT2 32  // sizeof(unsigned int)*2
+#   define SIZE_UINT3 48  // sizeof(unsigned int)*3
+#   define SIZE_UINT4 64  // sizeof(unsigned int)*4
+#   define SIZE_UINT5 80  // sizeof(unsigned int)*5
+#   define SIZE_UINT6 96  // sizeof(unsigned int)*6
+#   define MPI_TAG_CP_PASSUNEW 100
+#   define MPI_TAG_CP_ACCEPT 101
+#   define MPI_TAG_CP_REJECT 102
+#endif
+
+
+class param{
+    public:
+        param():p(1),tc(1),rank(0),ucur(1,0),unew(1,0),acceptvec(1,0),rejectvec(1,0) {}
+        param(size_t ip):p(ip),tc(1),rank(0),ucur(ip,0),unew(ip,0),acceptvec(ip,0),rejectvec(ip,0) {}
+        ~param(){} // destructor
+
+        // Objects
+        size_t p; // number of parameters
+        int tc;
+        size_t rank;
+        bool accept;
+        std::vector<double> ucur; // Current parameter vector u
+        std::vector<double> unew; // New parameter vector u
+        std::vector<size_t> acceptvec; // Accept counter
+        std::vector<size_t> rejectvec; // Reject counter
+        std::vector<std::string> propdistvec; // mh proposal distributions
+        std::vector<double> propvec; // mh proposal vector (distance or sd depending on the distribution)
+        std::vector<std::string> priordistvec; // Prior distributions
+        std::vector<double> priorp1vec; // Prior parameter 1
+        std::vector<double> priorp2vec; // Prior parameter 2 
+
+        // Methods
+        void adapt();
+        void drawnew(size_t ind,rn &gen); // Proposal for one parameter
+        void drawnew(rn &gen); // Joint proposal
+        void drawnew_mpi(size_t ind,rn &gen); // Proposal for one parameter mpi version
+        void drawnew_mpi(rn &gen); // Joint proposal mpi version
+        void getsumwr2_mpi(); // Get weight sum of square residuals
+        double lm(double wssr_cur, double wssr_new);
+        void mhstep(double cursumwr2, double newsumwr2, rn &gen);
+        void setmpirank(int rank) {this->rank = rank;}  //only needed for MPI
+        void setpriors(std::vector<std::string> prdist,std::vector<double> prp1, std::vector<double> prp2);
+        void setproposals(std::vector<std::string> propdist, std::vector<double> prop);
+        void settc(int tc) {this->tc = tc;}  // this is numslaves for MPI, or numthreads for OPEN_MP
+        void setucur(std::vector<double> uc){this->ucur = uc;this->unew = uc;}
+        void updatex(std::vector<double> &x, std::vector<size_t> ucols, size_t pxu, size_t n);
+
+   
+};
+
+
+#endif
