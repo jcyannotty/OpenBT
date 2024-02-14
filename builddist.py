0c3e826cd15e3203e6cd26fd1a82e4e4748cc1a2
John Yannotty <jcyannotty@gmail.com>
2024-02-13 20:09:14 -0500

    Try to push wheels to testpypi for ubuntu only

---
+++ builddist.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setup, find_packages
+cwd = os.getcwd()
+
+setup(
+    name='openbtmixing',
+    version='0.1',
+    packages=find_packages(where='openbtmixing'),
+    package_dir={'': 'openbtmixing'},
+    package_data={'': ['src/*.cpp', 'src/*.h']},
+)
