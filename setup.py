0dcba388d0308757c134b2babd8e6eaaf577548a
Jared O'Neal <joneal@anl.gov>
2024-05-01 16:39:25 -0500

    (Issue #2) Don't let setup.py determine macOS wheel name.

--- setup.py
+++ setup.py
@@ -56,8 +56,8 @@ if "linux_x86_64" in dist_name:
     dist_name = "manylinux2014_x86_64"
 
 
-if "macosx" in dist_name:
-    dist_name = "macosx_10_9_x86_64"
+#if "macosx" in dist_name:
+#    dist_name = "macosx_10_9_x86_64"
     #dist_name = "macosx_10_9_arm64"
 
 setup(
