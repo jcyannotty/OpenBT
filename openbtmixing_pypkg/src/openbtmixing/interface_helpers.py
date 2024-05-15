864bcc9ab43bcaa0c6245fc904d8c931a6734873
Jared O'Neal <joneal@anl.gov>
2024-05-15 13:59:39 -0500

    (Issue #2) Simplify and hack to see if it can run through oversubscribed.

--- openbtmixing_pypkg/src/openbtmixing/interface_helpers.py
+++ openbtmixing_pypkg/src/openbtmixing/interface_helpers.py
@@ -166,12 +166,12 @@ def run_model(fpath, tc, cmd="openbtcli", local_openbt_path = "", google_colab =
 
     # MPI with local program
     try:
-        subprocess.run(["mpirun", "-np", str(tc), cmd, str(fpath)],
-                       stdin=subprocess.DEVNULL,
-                       capture_output=True, check=True)
-        #subprocess.run(["mpirun", "-np", str(tc), "--oversubscribe", cmd, str(fpath)],
+        #subprocess.run(["mpirun", "-np", str(tc), cmd, str(fpath)],
         #               stdin=subprocess.DEVNULL,
         #               capture_output=True, check=True)
+        subprocess.run(["mpirun", "-np", str(tc), "--oversubscribe", cmd, str(fpath)],
+                       stdin=subprocess.DEVNULL,
+                       capture_output=True, check=True)
     except subprocess.CalledProcessError as err:
         stdout = err.stdout.decode()
         stderr = err.stderr.decode()
