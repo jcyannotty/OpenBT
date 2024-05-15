01db1bae47555279acc86f033735dd99831c9e2a
Jared O'Neal <joneal@anl.gov>
2024-05-15 16:31:25 -0500

    (Issue #2) Move Python setup out to script.
    With these, changes I am happy with the state of this CI spec.

---
+++ .github/workflows/setup_base_python.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo
+    echo "Please pass GitHub action runner OS (e.g., Linux or macOS)"
+    echo
+    exit 1
+fi
+runner_os=$1
+
+which python
+which pip
+echo " "
+python -c "import platform ; print(platform.machine())"
+python -c "import platform ; print(platform.system())"
+python -c "import platform ; print(platform.release())"
+python -c "import platform ; print(platform.platform())"
+python -c "import platform ; print(platform.version())"
+if   [ "$runner_os" = "macOS" ]; then
+    python -c "import platform ; print(platform.mac_ver())"
+    echo " "
+    # Get information on number of processors, which sets limit on number of
+    # "slots" available for MPI processes in OpenMPI.
+    sysctl hw.ncpu
+    sysctl hw.physicalcpu
+    sysctl hw.logicalcpu
+elif [ "${{ runner.os }}" = "Linux" ]; then
+    echo " "
+    # Get information on number of cores & hardware threads, which sets limit
+    # on number of "slots" available for MPI processes in OpenMPI.
+    lscpu
+else
+    echo
+    echo "Invalid runner OS $runner_os"
+    echo
+    exit 1
+fi
+echo " "
+python -m pip install --upgrade pip
+python -m pip install --upgrade setuptools
+python -m pip install build
+python -m pip install tox
+echo " "
+python --version
+tox --version
+echo " "
+pip list
+echo " "
