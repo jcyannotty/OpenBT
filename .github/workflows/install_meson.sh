ea7f06d1c2e63327e0772b51e6474e017d12702a
Jared O'Neal <joneal@anl.gov>
2024-12-04 12:16:38 -0600

    (Issue #6) Get meson via brew & see if we can skip mpi4py install.

--- .github/workflows/install_meson.sh
+++ .github/workflows/install_meson.sh
@@ -14,33 +14,34 @@ meson_venv=$venv_path/meson
 local_bin=$install_path/local/bin
 
 if   [ "$runner_os" = "macOS" ]; then
-    brew install ninja
+    # Homebrew already has v1.6.0 available.
+    brew install meson
 elif [ "$runner_os" = "Linux" ]; then
     sudo apt-get update
     sudo apt-get -y install ninja-build
+
+    echo " "
+    mkdir -p $venv_path
+    mkdir -p $local_bin
+    
+    # Beginning with v1.6.0 meson can automatically find OpenMPI and MPICH
+    python -m venv $meson_venv
+    . $meson_venv/bin/activate
+    which python
+    which pip
+    python -m pip install --upgrade pip
+    python -m pip install meson>=1.6.0
+    echo " "
+    python -m pip list
+    echo " "
+    
+    # Install just meson command in path
+    ln -s $meson_venv/bin/meson $local_bin
+    echo "$local_bin" >> "$GITHUB_PATH"
+    echo " "
 else
     echo
     echo "Invalid runner OS $runner_os"
     echo
     exit 1
 fi
-
-echo " "
-mkdir -p $venv_path
-mkdir -p $local_bin
-
-# Beginning with v1.6.0 meson can automatically find OpenMPI and MPICH
-python -m venv $meson_venv
-. $meson_venv/bin/activate
-which python
-which pip
-python -m pip install --upgrade pip
-python -m pip install meson>=1.6.0
-echo " "
-python -m pip list
-echo " "
-
-# Install just meson command in path
-ln -s $meson_venv/bin/meson $local_bin
-echo "$local_bin" >> "$GITHUB_PATH"
-echo " "
